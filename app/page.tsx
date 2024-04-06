f795f8bdfe1a6460a6ce507198401997c909f987
Aleksandër Pulla <aleksanderpulla@gmail.com>
2024-04-06 16:52:35 +0200

    jj

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function DashboardPage() {
       <div className="hidden flex-col md:flex">
         <div className="border-b">
           <div className="flex h-16 items-center px-8">
-            <h2 className="text-3xl font-extralight tracking-tight dark:text-emerald-300 text-emerald-500">
+            <h2 className="text-3xl dark:font-extralight tracking-tight dark:text-emerald-300 text-emerald-600 font-light">
               EnergyIQ
             </h2>
             <div className="ml-auto flex items-center space-x-4">
@@ -30,7 +30,6 @@ export default function DashboardPage() {
         <div className="flex-1 space-y-4 p-8 pt-6">
           <div className="flex items-center justify-between space-y-2">
             <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-            <div className="flex items-center space-x-2"></div>
           </div>
           <Tabs defaultValue="overview" className="space-y-4">
             <TabsList>
